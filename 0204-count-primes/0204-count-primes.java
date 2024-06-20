ad85d55807ad305441e34266611c0b2ba0a92a09
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-20 12:16:12 +0530

    Time: 123 ms (24.87%), Space: 48.9 MB (75%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,30 +1,17 @@
 class Solution {
-    public int countPrimes(int n) {
-        if(n <=1){
-            return 0;
-        }
-        int count=0;
-
-        for(int i =2; i< n; i++){
-            if(isPrime(i)){
-                count++;
-            }
-        }
-        return count;
-      
-    }
-
-
-
-    public boolean isPrime(int n){
-          if(n <=1 ){
-            return false;
-          }
-          for(int i=2; i*i<=n; i++){
-            if(n % i==0){
-                return false;
-            }
-          }
-          return true;
-    }
+        public int countPrimes(int n) {
+                boolean[] primes = new boolean[n];
+                        Arrays.fill(primes, true);
+                                int result = 0;
+                                        for (int i = 2; i < n; ++i) {
+                                                  if (primes[i]) {
+                                                                    result++;
+                                                                                    for (int j = i + i; j < n; j += i) {
+                                                                                                        primes[j] = false;
+                                                                                                                        }
+                                                                                                                                    }
+                                                                                                                                            }
+                                                                                                                                                    return result;
+                                                                                                                                                        }
+                                                                                                                                                        
 }
