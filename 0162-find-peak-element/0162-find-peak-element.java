96cbd77c8b16f13eaba330fe60e35b01ae032cb1
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-21 21:08:27 +0530

    Time: 0 ms (100%), Space: 42.2 MB (59.18%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,6 +3,7 @@ class Solution {
     
         int ans =0;
         int i=0;
+         if (nums.length == 1) return 0;
            if (nums[0] > nums[1]) return 0;
                    if (nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
         for( i=1; i< nums.length-1; i++){
