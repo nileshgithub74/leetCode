9d7f71837ffd875786488311e3d8534ca2994f11
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2025-07-22 15:17:02 +0530

    Time: 2 ms (97.99%), Space: 44.2 MB (17.31%) - LeetHub

---
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double minimumAverage(int[] arr) {
+        Arrays.sort(arr);
+        double ans = Integer.MAX_VALUE;
+
+        int i =0;
+        int j = arr.length-1;
+        while(i  <j){
+         double  average =(arr[i]+ arr[j])/2.0;
+            ans = Math.min(average, ans);
+            i++;
+            j--;
+        }
+        
+     return ans;
+    }
+}
