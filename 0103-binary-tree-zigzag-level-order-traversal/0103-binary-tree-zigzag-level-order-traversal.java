74ab6dca7de9d598fa24dbf7d4a326949205aadc
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2025-12-23 12:38:07 +0530

    Time: 1 ms (79.37%), Space: 43.6 MB (46.74%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,46 +16,55 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
+        // we will maintain two stack one for lefttoright and another for righttoleft traversal
+
         List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
 
-        if(root ==null)return result;
+        Stack<TreeNode> st = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        st.push(root);
+        while (!st.isEmpty() || !st2.isEmpty()) {
 
-        boolean leftToright =true;
+            List<Integer> level = new ArrayList<>();
 
-        while(!q.isEmpty()){
-            int n = q.size();
-           
-        List<Integer> list = new ArrayList<>();
+            while (!st.isEmpty()) {
+                TreeNode curr = st.pop();
+                level.add(curr.val);
 
-            for(int i =0; i< n; i++){
-                TreeNode curr = q.poll();
-
-                if(leftToright){
-                    list.addLast(curr.val);
-                }else{
-                    list.addFirst(curr.val);
-                }
-
-
-                if(curr.left != null)q.add(curr.left);
-                if(curr.right != null)q.add(curr.right);
-
-
+                if (curr.left != null)
+                    st2.push(curr.left);
+                if (curr.right != null)
+                    st2.push(curr.right);
 
             }
 
-            result.add(list);
-            leftToright = !leftToright;
+            if(!level.isEmpty())result.add(level);
+
+            List<Integer> level2 = new ArrayList<>();
+
+            while(!st2.isEmpty()){
+                TreeNode curr =st2.pop();
+                level2.add(curr.val);
+
+
+                if(curr.right != null) st.push(curr.right);
+                if(curr.left != null)st.push(curr.left);
+            }
+
+            if(!level2.isEmpty())result.add(level2);
+
+
+
+
+
+
 
 
         }
         return result;
 
-
-
-        
     }
 }
