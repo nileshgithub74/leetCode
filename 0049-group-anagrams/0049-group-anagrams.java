e5742cf34461061982a4a8e0af98ec0dfb04d060
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-02-09 20:08:04 +0530

    Time: 18 ms (26.95%), Space: 49.9 MB (48.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,26 +1,40 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-    Map<String, List<String> > map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
 
-    for(String s : strs){
-        char[] ch = s.toCharArray();
-        Arrays.sort(ch);
-        String sorted = new String(ch);
+        for (String str : strs) {
 
-        if(!map.containsKey(sorted)){
-            map.put(sorted, new ArrayList<>());
+            int[] freq =  new int[26];
+
+            for(int i=0; i <str.length(); i++){
+                freq[str.charAt(i)-'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int x : freq){
+                sb.append(x).append('#');
+            }
+
+            String sorted = sb.toString();
+
+
+
+           
+
+
+           
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(str);
+
         }
-        map.get(sorted).add(s);
 
+        return new ArrayList<>(map.values());
 
-
-
-    }
-
-    return new ArrayList<>(map.values());
-
-
-        
     }
 }
