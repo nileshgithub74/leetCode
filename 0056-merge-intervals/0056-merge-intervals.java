32efcf3088ec51ffc260dff57c6c43ac56552ae0
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-02-10 11:36:24 +0530

    Time: 11 ms (17.87%), Space: 49.3 MB (37.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,40 +1,44 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
+    
+       List<int[]> result = new ArrayList<>();
+       int n = intervals.length;
+       
+       // sort the arrrays
 
-        List<int[]> result = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> {
-            return Integer.compare(a[0], b[0]);
-        });
+       Arrays.sort(intervals, (a, b)-> a[0]-b[0]);
 
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+       int start = intervals[0][0];
+       int end = intervals[0][1];
 
-        for (int i = 1; i < n; i++) {
+       for(int i =1; i <n; i++){
 
-            int nextstart = intervals[i][0];
-            int nextend = intervals[i][1];
-
-            if (nextstart <= end) {
-                end = Math.max(end, nextend);
-            }
-
-            else {
-                result.add(new int[] { start, end });
-                start = nextstart;
-                end = nextend;
-            }
-
+        int nextstart = intervals[i][0];
+        int nextend = intervals[i][1];
+        
+        if(nextstart <= end){
+            end= Math.max(end, nextend);
+        }else{
+            result.add(new int[]{start, end});
+            start = nextstart;
+            end = nextend;
         }
+        
+          
+       }
 
-        result.add(new int[] { start, end });
-        int[][] ans = new int[result.size()][2];
+       result.add(new int[]{start, end});
 
-        for (int i = 0; i < result.size(); i++) {
-            ans[i] = result.get(i);
-        }
 
-        return ans;
+       int[][] list =new int[result.size()][2];
 
+       for(int i=0; i< result.size(); i++){
+          list[i] = result.get(i);
+       }
+
+       return list;
+
+
+        
     }
 }
