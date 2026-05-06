27179e7bfe2e4110d61a39d9c50726dc0e3a14e0
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-05-06 13:18:11 +0530

    Time: 1 ms (95.5%), Space: 46.8 MB (61.06%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,47 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        List<List<Integer>> res = new ArrayList<>();
-          if (root == null) return res;
 
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result ;
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
         while(!q.isEmpty()){
 
-            int size= q.size();
-            List<Integer> curr = new ArrayList<>();
+            int size =q.size();
+            List<Integer>  list = new ArrayList<>();
+            for(int i=0; i< size; i++){
 
-            for(int i =0; i< size; i++){
-                TreeNode removed = q.poll();
-                curr.add(removed.val);
+                TreeNode curr  = q. poll();
+                list.add(curr.val);
 
-                if(removed.left != null){
-                    q.add(removed.left);
-                }
-                if(removed.right != null){
-                    q.add(removed.right);
-                }
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+                
+
             }
-            res.add(curr);
-
-
-            
+            result.add(list);
         }
-        return res;
+        return result;
+        
         
     }
 }
