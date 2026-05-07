30f72743ad115883fa5935e76d4a660372edfced
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-05-07 18:09:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,16 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-
-      int n = arr.length;
-      HashMap<Integer, Integer> map = new HashMap<>();
-
-
-
-      for(int i =0; i <n;i++){
-        int comp  = target - arr[i];
-
-        if(map.containsKey(comp)){
-            return new int[]{i, map.get(comp)};
+        int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i< n; i++){
+            int required = target-arr[i];
+            if(map.containsKey(required)){
+                return new int[]{  map.get(required), i};
+            }else{
+                map.put(arr[i], i);
+            }
         }
-       map.put(arr[i], i);
-        
-         
-      }
-      return new int[]{-1,-1};
-        
+        return new int[]{-1, -1};
     }
 }
