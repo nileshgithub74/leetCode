3256a3e40e1cc9b9b4855b2d20741b91b048c1af
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-05-20 00:27:06 +0530

    Time: 0 ms (100%), Space: 45 MB (70.9%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+
+        if(n<2)return true;
+
+        for(int i =1; i< nums.length; i++){
+            if(nums[i-1] % 2== 0 && nums[i] % 2 ==1  || nums[i-1] %2 ==1 && nums[i]%2 == 0){
+            continue;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+}
